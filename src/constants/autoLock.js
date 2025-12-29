c7936e0350b481bb9abd97fc1ee854882c29e6bc
Denis Lupookov <denislupookov@gmail.com>
2025-12-29 13:12:39 +0100

    updated auto lock to use values from time.js

--- src/constants/autoLock.js
+++ src/constants/autoLock.js
@@ -1,11 +1,13 @@
+import { MS_PER_SECOND, MS_PER_MINUTE, MS_PER_HOUR } from './time.js'
+
 export const AUTO_LOCK_TIMEOUT_OPTIONS = {
-  SECONDS_30: { label: '30 seconds', value: 30 * 1000 },
-  MINUTES_1: { label: '1 minute', value: 60 * 1000 },
-  MINUTES_5: { label: '5 minutes', value: 5 * 60 * 1000 },
-  MINUTES_15: { label: '15 minutes', value: 15 * 60 * 1000 },
-  MINUTES_30: { label: '30 minutes', value: 30 * 60 * 1000 },
-  HOURS_1: { label: '1 hour', value: 60 * 60 * 1000 },
-  HOURS_4: { label: '4 hours', value: 4 * 60 * 60 * 1000 }
+  SECONDS_30: { label: '30 seconds', value: 30 * MS_PER_SECOND },
+  MINUTES_1: { label: '1 minute', value: MS_PER_MINUTE },
+  MINUTES_5: { label: '5 minutes', value: 5 * MS_PER_MINUTE },
+  MINUTES_15: { label: '15 minutes', value: 15 * MS_PER_MINUTE },
+  MINUTES_30: { label: '30 minutes', value: 30 * MS_PER_MINUTE },
+  HOURS_1: { label: '1 hour', value: MS_PER_HOUR },
+  HOURS_4: { label: '4 hours', value: 4 * MS_PER_HOUR }
 }
 
 export const DEFAULT_AUTO_LOCK_TIMEOUT =
