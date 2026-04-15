8c86fcb45f4f57050dd299c49d2b7376fc95abc5
Denis Lupookov <denislupookov@gmail.com>
2026-04-15 21:03:07 +0800

    Enable design version 2

--- src/constants/flags.js
+++ src/constants/flags.js
@@ -5,5 +5,5 @@ export const BE_AUTO_LOCK_ENABLED = true
 export const SAVE_CREDENTIALS_AFTER_LOGIN_ENABLED = false
 export const DELETE_VAULT_ENABLED = false
 export const AUTHENTICATOR_ENABLED = false
-export const DESIGN_VERSION = 1
+export const DESIGN_VERSION = 2
 export const UNSUPPORTED = true
